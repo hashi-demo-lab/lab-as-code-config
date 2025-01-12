93b8903e63a5bf34c38ae06eef90872242225d29
Aaron Evans <aarone@outlook.com.au>
2025-01-13 10:33:39 +1100

    Add variables for Kubernetes RoleBindings, Roles, and ServiceAccounts manifests

---
+++ modules/kubernetes/variables.tf
@@ -0,0 +1,14 @@
+variable "role_bindings" {
+  description = "Content of the RoleBindings manifest"
+  type        = string
+}
+
+variable "roles" {
+  description = "Content of the Roles manifest"
+  type        = string
+}
+
+variable "serviceaccounts" {
+  description = "Content of the ServiceAccounts manifest"
+  type        = string
+}
