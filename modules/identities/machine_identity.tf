b0f02765cbde8e7f6b660c22cf4df8cda9c2bdbf
Aaron Evans <aarone@outlook.com.au>
2025-01-30 04:15:39 +1100

    Add machine identity entities and aliases for application 4

---
+++ modules/identities/machine_identity.tf
@@ -0,0 +1,52 @@
+resource "vault_identity_entity" "app4_web_entity" {
+  name     = "app4-web"
+  policies = []
+  metadata = {
+    type = "machine"
+    app  = "4"
+    function = "web"
+  }
+}
+
+resource "vault_identity_entity_alias" "alias_app4_web" {
+  name           = "app4-web"                 # must match the JWT claim
+  mount_accessor = var.k8s_jwt_accessor
+  canonical_id   = vault_identity_entity.app4_web_entity.id
+}
+
+
+
+
+resource "vault_identity_entity" "app4_api_entity" {
+  name     = "app4-api"
+  policies = []
+  metadata = {
+    type = "machine"
+    app  = "4"
+    function = "api"
+  }
+}
+
+resource "vault_identity_entity_alias" "alias_app4_api" {
+  name           = "app4-api"                 # must match the JWT claim
+  mount_accessor = var.k8s_jwt_accessor
+  canonical_id   = vault_identity_entity.app4_api_entity.id
+}
+
+
+
+resource "vault_identity_entity" "app4_db_entity" {
+  name     = "app4-db"
+  policies = []
+  metadata = {
+    type = "machine"
+    app  = "4"
+    function = "db"
+  }
+}
+
+resource "vault_identity_entity_alias" "alias_app4_db" {
+  name           = "app4-db"                 # must match the JWT claim
+  mount_accessor = var.k8s_jwt_accessor
+  canonical_id   = vault_identity_entity.app4_db_entity.id
+}
