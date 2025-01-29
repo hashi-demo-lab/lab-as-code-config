8fafe0bf7eb5d431483bb49be973bb052dca1320
Aaron Evans <aarone@outlook.com.au>
2025-01-30 04:16:05 +1100

    Refactor Kubernetes manifest handling to read multiple YAML files and create resources dynamically

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,11 +1,23 @@
-resource "kubernetes_manifest" "vault_auth_test" {
-  manifest = yamldecode(var.role_bindings)
+# Get all YAML files in the specified directory
+locals {
+  manifest_files = fileset(var.manifest_directory, "*.yaml")
 }
 
-resource "kubernetes_manifest" "vault_auth_ldap" {
-  manifest = yamldecode(var.roles)
+# Read and decode each manifest file
+locals {
+  decoded_manifests = flatten([
+    for file in local.manifest_files : provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}"))
+  ])
 }
 
-resource "kubernetes_manifest" "vault_auth_db" {
-  manifest = yamldecode(var.serviceaccounts)
+# Create Kubernetes resources for each manifest
+resource "kubernetes_manifest" "resources" {
+  for_each = { for idx, manifest in local.decoded_manifests : idx => manifest }
+  
+  manifest = each.value
+}
+
+# Output the applied resources
+output "applied_manifests" {
+  value = { for k, v in kubernetes_manifest.resources : k => v.object }
 }
