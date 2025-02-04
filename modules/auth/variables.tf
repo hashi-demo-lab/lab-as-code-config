822c6cc202f3276a3540f00015aaf4d7575d5b50
Aaron Evans <aarone@outlook.com.au>
2025-02-05 06:30:25 +1100

    Add kube_config_path variable to authentication module

--- modules/auth/variables.tf
+++ modules/auth/variables.tf
@@ -7,3 +7,9 @@ variable "oidc_client_secret" {
   description = "The OIDC client secret"
   type = string
 }
+
+variable "kube_config_path" {
+  description = "Path to the kubeconfig file"
+  type        = string
+  default     = "~/.kube/config"
+}
