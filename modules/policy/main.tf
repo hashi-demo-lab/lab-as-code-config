bff9264832677494b107c6ec263921410513036a
Aaron Evans <aarone@outlook.com.au>
2025-02-05 06:31:18 +1100

    Refactor Vault policy resource definitions to use dynamic file loading for improved maintainability

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -1,24 +1,9 @@
-resource "vault_policy" "aarons_policy" {
-  name   = "user-aaron-access"
-  policy = file("${path.module}/policies/aaron-access.hcl")
+locals {
+  policy_files = fileset("${path.module}/policies", "*.hcl")
 }
 
-resource "vault_policy" "aarons_privileged_policy" {
-  name   = "user-aaron-privileged-access"
-  policy = file("${path.module}/policies/aaron-privileged-access.hcl")
-}
-
-resource "vault_policy" "simons_policy" {
-  name   = "user-simon-access"
-  policy = file("${path.module}/policies/simon-access.hcl")
-}
-
-resource "vault_policy" "sea_policy" {
-  name   = "team-sea-access"
-  policy = file("${path.module}/policies/sea-access.hcl")
-}
-
-resource "vault_policy" "admin_approval_policy" {
-  name   = "admin-approval-access"
-  policy = file("${path.module}/policies/admin-approval.hcl")
+resource "vault_policy" "all_policies" {
+  for_each = { for file in local.policy_files : file => file }
+  name     = replace(each.key, ".hcl", "")
+  policy   = file("${path.module}/policies/${each.value}")
 }
