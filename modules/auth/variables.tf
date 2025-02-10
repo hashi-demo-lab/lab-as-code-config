c23786d0a162f11987a1c78cd6a212d47ee1059a
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:53:21 +1100

    Refactor variable definitions in auth module for consistency and add intermediate CA certificate variable

--- modules/auth/variables.tf
+++ modules/auth/variables.tf
@@ -1,15 +1,20 @@
 variable "oidc_client_id" {
   description = "The OIDC client ID"
-  type = string
+  type        = string
 }
 
 variable "oidc_client_secret" {
   description = "The OIDC client secret"
-  type = string
+  type        = string
 }
 
 variable "kube_config_path" {
   description = "Path to the kubeconfig file"
   type        = string
   default     = "~/.kube/config"
+}
+
+variable "intermediate_ca" {
+  description = "The intermediate CA certificate"
+  type        = string
 }
