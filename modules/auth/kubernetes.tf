2bb8929b7d0e5f327a5d7b60c8ad1a2a0f26cf8e
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:50:56 +1100

    Rename Kubernetes auth backend config resource and add role definitions for hashibank-web and app5

--- modules/auth/kubernetes.tf
+++ modules/auth/kubernetes.tf
@@ -7,14 +7,24 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-resource "vault_kubernetes_auth_backend_config" "this" {
+resource "vault_kubernetes_auth_backend_config" "kubernetes_config" {
   backend            = vault_auth_backend.kubernetes.path
   kubernetes_host    = local.cluster.server
   kubernetes_ca_cert = base64decode(local.cluster["certificate-authority-data"])
   # token_reviewer_jwt omitted as it is optional
 }
 
-resource "vault_kubernetes_auth_backend_role" "this" {
+resource "vault_kubernetes_auth_backend_role" "hashibank-web" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "hashibank-web"
+  bound_service_account_names      = ["*"]
+  bound_service_account_namespaces = ["hashibank"]
+  token_ttl                        = 259200
+  token_policies                   = ["request-cert"]
+  alias_name_source                = "serviceaccount_name"
+}
+
+resource "vault_kubernetes_auth_backend_role" "app5" {
   backend                          = vault_auth_backend.kubernetes.path
   role_name                        = "app5"
   bound_service_account_names      = ["*"]
