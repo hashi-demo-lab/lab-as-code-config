3ce352466620b9c9eba0c3441680a038575c169f
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:54:38 +1100

    Refactor main.tf to integrate intermediate CA certificate from secret engines module and improve accessor variable naming

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-module "auth" {
-  source = "./modules/auth"
-
-  oidc_client_id     = var.oidc_client_id
-  oidc_client_secret = var.oidc_client_secret
-}
-
-module "identities" {
-  source     = "./modules/identities"
-  depends_on = [module.auth]
-
-  ldap_accessor    = module.auth.ldap_accessor
-  oidc_accessor    = module.auth.oidc_accessor
-  k8s_jwt_accessor = module.auth.k8s_jwt_accessor
-}
-
-module "policies" {
-  source = "./modules/policy"
-
-  depends_on = [module.auth]
-}
-
 module "secret_engines" {
   source = "./modules/secret_engines"
 
@@ -27,6 +5,31 @@ module "secret_engines" {
   team_secrets_path   = var.team_secrets_path
   aarons_secrets_path = var.aarons_secrets_path
   simons_secrets_path = var.simons_secrets_path
+}
+
+module "auth" {
+  source = "./modules/auth"
+
+  oidc_client_id     = var.oidc_client_id
+  oidc_client_secret = var.oidc_client_secret
+  intermediate_ca = module.secret_engines.intermediate_ca_cert_pem
+}
+
+module "identities" {
+  source     = "./modules/identities"
+  depends_on = [module.auth]
+
+  ldap_accessor       = module.auth.ldap_accessor
+  oidc_accessor       = module.auth.oidc_accessor
+  jwt_accessor        = module.auth.jwt_accessor
+  kubernetes_accessor = module.auth.kubernetes_accessor
+  tfc_accessor        = module.auth.tfc_accessor
+}
+
+module "policies" {
+  source = "./modules/policy"
+
+  depends_on = [module.auth]
 }
 
 module "kubernetes_resources" {
