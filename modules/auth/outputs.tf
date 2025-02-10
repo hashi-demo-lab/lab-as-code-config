5da8cd0f257fc8da669dac299aef3579e6dde92f
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:54:08 +1100

    Refactor auth module outputs for consistency and clarity

--- modules/auth/outputs.tf
+++ modules/auth/outputs.tf
@@ -1,11 +1,19 @@
 output "ldap_accessor" {
-    value = vault_ldap_auth_backend.ldap.accessor
+  value = vault_ldap_auth_backend.ldap.accessor
 }
 
 output "oidc_accessor" {
-    value = vault_jwt_auth_backend.oidc.accessor
+  value = vault_jwt_auth_backend.oidc.accessor
 }
 
-output "k8s_jwt_accessor" {
-    value = vault_jwt_auth_backend.jwt.accessor
+output "jwt_accessor" {
+  value = vault_jwt_auth_backend.jwt.accessor
+}
+
+output "kubernetes_accessor" {
+  value = vault_auth_backend.kubernetes.accessor
+}
+
+output "tfc_accessor" {
+  value = vault_jwt_auth_backend.tfc.accessor
 }
