6bc9b532f1dcbe3a90bfbb36f5a27c30f73c742d
Aaron Evans <aarone@outlook.com.au>
2025-03-11 14:19:59 +1100

    Add AppRole authentication backend and create test-role with access-all policies

---
+++ modules/auth/approle.tf
@@ -0,0 +1,12 @@
+resource "vault_auth_backend" "approle" {
+  type       = "approle"
+  path       = "approle"
+}
+
+# Create a role named, "test-role"
+resource "vault_approle_auth_backend_role" "test-role" {
+  depends_on     = [vault_auth_backend.approle]
+  backend        = vault_auth_backend.approle.path
+  role_name      = "test-role"
+  token_policies = ["default", "access-all"]
+}
