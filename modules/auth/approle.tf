70668f155a44b6099fbb89ea4174d552902a5fa5
Aaron Evans <aarone@outlook.com.au>
2025-03-11 21:43:46 +1100

    Refactor approle configuration: streamline type and path definitions, and update token policies for test-role

--- modules/auth/approle.tf
+++ modules/auth/approle.tf
@@ -1,6 +1,6 @@
 resource "vault_auth_backend" "approle" {
-  type       = "approle"
-  path       = "approle"
+  type = "approle"
+  path = "approle"
 }
 
 # Create a role named, "test-role"
@@ -8,5 +8,5 @@ resource "vault_approle_auth_backend_role" "test-role" {
   depends_on     = [vault_auth_backend.approle]
   backend        = vault_auth_backend.approle.path
   role_name      = "test-role"
-  token_policies = ["default", "access-all"]
+  token_policies = ["access-all"]
 }
