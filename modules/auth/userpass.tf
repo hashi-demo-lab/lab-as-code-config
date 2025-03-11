bc6751fb699c104d829fd526273bf7c41c31063a
Aaron Evans <aarone@outlook.com.au>
2025-03-11 21:51:11 +1100

    Remove unused local user resource for aaron in userpass authentication module

--- modules/auth/userpass.tf
+++ modules/auth/userpass.tf
@@ -2,16 +2,3 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
   path = "userpass"
 }
-
-# Create local users
-resource "vault_generic_endpoint" "aaron" {
-  depends_on           = [vault_auth_backend.userpass]
-  path                 = "auth/userpass/users/aaron"
-  ignore_absent_fields = true
-  data_json            = <<EOT
-{
-  "policies": ["access-all"],
-  "password": "changeme"
-}
-EOT
-}
