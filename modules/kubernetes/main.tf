78f27b9348eb38a7c4718ad474bb3f75e124d533
Aaron Evans <aarone@outlook.com.au>
2025-03-11 14:22:07 +1100

    Refactor Kubernetes module: comment out manifest processing logic

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,29 +1,26 @@
-# Get all YAML files in the specified directory
-locals {
-  manifest_files = fileset(var.manifest_directory, "*.yaml")
-}
+# # Get all YAML files in the specified directory
+# locals {
+#   manifest_files = fileset(var.manifest_directory, "*.yaml")
+# }
 
-# Read and decode each manifest file
-locals {
-  decoded_manifests = flatten([
-    for file in local.manifest_files : provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}"))
-  ])
-}
+# # Read and decode each manifest file
+# locals {
+#   decoded_manifests = flatten([
+#     for file in local.manifest_files : provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}"))
+#   ])
+# }
 
-# Create Kubernetes resources for each manifest
-resource "kubernetes_manifest" "resources" {
-  for_each = {
-    for manifest in local.decoded_manifests :
-    "${manifest.kind}-${lookup(manifest.metadata, "namespace", "default")}-${manifest.metadata.name}" => manifest
-  }
+# # Create Kubernetes resources for each manifest
+# resource "kubernetes_manifest" "resources" {
+#   for_each = {
+#     for manifest in local.decoded_manifests :
+#     "${manifest.kind}-${lookup(manifest.metadata, "namespace", "default")}-${manifest.metadata.name}" => manifest
+#   }
 
-  manifest = each.value
-}
+#   manifest = each.value
+# }
 
-
-
-
-# Output the applied resources
-output "applied_manifests" {
-  value = { for k, v in kubernetes_manifest.resources : k => v.object }
-}
+# # Output the applied resources
+# output "applied_manifests" {
+#   value = { for k, v in kubernetes_manifest.resources : k => v.object }
+# }
