9b070cfa7012d9e557e3a8e69837baf9e696cb7e
Aaron Evans <aarone@outlook.com.au>
2025-03-14 09:07:06 +1100

    Remove unused Vault authentication configuration

--- modules/work-in-progress/vault_auth.tf
+++
@@ -1,9 +0,0 @@
-/*resource "vault_github_auth_backend" "github_auth" {
-  depends_on = [ helm_release.vault ]
-  organization = var.github_organization
-}
-
-resource "vault_auth_backend" "userpass" {
-  depends_on = [ helm_release.vault ]
-  type = "userpass"
-}*/
