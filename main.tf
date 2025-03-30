f886f6db8e9fb86b806df7b780de0fa2b6324433
Aaron Evans <aarone@outlook.com.au>
2025-03-31 10:25:57 +1100

    Refactor secret_engines module to correctly assign root_ca and root_ca_key; streamline identities module dependencies

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ module "secret_engines" {
   team_secrets_path   = var.team_secrets_path
   aarons_secrets_path = var.aarons_secrets_path
   simons_secrets_path = var.simons_secrets_path
+  root_ca             = data.local_file.root_ca_cert.content
+  root_ca_key         = data.local_file.root_ca_key.content
 }
 
 module "auth" {
@@ -16,13 +18,11 @@ module "auth" {
 
   oidc_client_id     = var.oidc_client_id
   oidc_client_secret = var.oidc_client_secret
-  intermediate_ca    = data.local_file.root_ca_cert.content
+  root_ca    = data.local_file.root_ca_cert.content
 }
 
 module "identities" {
-  source     = "./modules/identities"
-  depends_on = [module.auth]
-
+  source              = "./modules/identities"
   ldap_accessor       = module.auth.ldap_accessor
   oidc_accessor       = module.auth.oidc_accessor
   jwt_accessor        = module.auth.jwt_accessor
@@ -31,6 +31,8 @@ module "identities" {
   kubernetes_accessor = module.auth.kubernetes_accessor
   tfc_accessor        = module.auth.tfc_accessor
   cert_accessor       = module.auth.cert_accessor
+
+  depends_on = [module.auth]
 }
 
 module "policies" {
