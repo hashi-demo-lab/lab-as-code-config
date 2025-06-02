e6a23a68b660111c13becbed7345deae9d81b897
Aaron Evans <aarone@outlook.com.au>
2025-06-02 12:20:53 +1000

    Refactor Kubernetes auth backend configuration to use specific Docker Desktop cluster details and add audience to app5 role

--- modules/auth/kubernetes.tf
+++ modules/auth/kubernetes.tf
@@ -1,6 +1,11 @@
 locals {
   kubeconfig = yamldecode(file(var.kube_config_path))
-  cluster    = local.kubeconfig.clusters[0].cluster
+  docker_desktop_cluster = one([
+    for cluster in local.kubeconfig.clusters : cluster
+    if cluster.name == "docker-desktop"
+  ])
+  docker_desktop_server   = "https://kubernetes.default.svc.cluster.local"
+  docker_desktop_ca_cert  = base64decode(local.docker_desktop_cluster.cluster["certificate-authority-data"])
 }
 
 resource "vault_auth_backend" "kubernetes" {
@@ -9,8 +14,8 @@ resource "vault_auth_backend" "kubernetes" {
 
 resource "vault_kubernetes_auth_backend_config" "kubernetes_config" {
   backend            = vault_auth_backend.kubernetes.path
-  kubernetes_host    = local.cluster.server
-  kubernetes_ca_cert = base64decode(local.cluster["certificate-authority-data"])
+  kubernetes_host    = local.docker_desktop_server
+  kubernetes_ca_cert = local.docker_desktop_ca_cert
   # token_reviewer_jwt omitted as it is optional
 }
 
@@ -29,6 +34,7 @@ resource "vault_kubernetes_auth_backend_role" "app5" {
   role_name                        = "app5"
   bound_service_account_names      = ["*"]
   bound_service_account_namespaces = ["app5"]
+  audience                         = "vault"
   token_ttl                        = 259200
   token_policies                   = ["development"]
 }
