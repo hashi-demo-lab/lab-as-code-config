1b78573f10198e17b64c2dd1892590dec6411c7a
Aaron Evans <aarone@outlook.com.au>
2025-06-02 12:23:02 +1000

    Add enforcement policy for OIDC TTL limits with mandatory enforcement level

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -7,3 +7,10 @@ resource "vault_policy" "all_policies" {
   name     = replace(each.key, ".hcl", "")
   policy   = file("${path.module}/policies/${each.value}")
 }
+
+resource "vault_egp_policy" "enforce_ttl_limits" {
+  name              = "enforce-ttl-limits"
+  paths             = ["sys/auth/oidc/tune"]
+  enforcement_level = "hard-mandatory"
+  policy            = file("${path.module}/policies/sentinel/enforce_ttl_limits.sentinel")
+}
