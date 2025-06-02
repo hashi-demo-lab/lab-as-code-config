d4b41d18d4aac83b76657f20ee69b2f6535ee28f
Aaron Evans <aarone@outlook.com.au>
2025-06-02 12:24:10 +1000

    Refactor manifest handling in Kubernetes module to improve clarity and remove unused code

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,27 +1,40 @@
-# Get all YAML files in the specified directory
-locals {
-  manifest_files = fileset(var.manifest_directory, "*.yaml")
+# # Get all YAML files in the specified directory
+# locals {
+#   manifest_files = fileset(var.manifest_directory, "*.yaml")
   
-  # Convert manifest_files set to a map with all values set to true
-  manifest_files_map = {
-    for file in local.manifest_files : file => true
-  }
+#   # Convert manifest_files set to a map with all values set to true
+#   manifest_files_map = {
+#     for file in local.manifest_files : file => true
+#   }
   
-  # Determine which manifests to deploy based on enable_manifests variable
-  # If enable_manifests is empty, use all manifests from manifest_files_map
-  # Otherwise, overlay the enable_manifests values onto manifest_files_map
-  manifests_to_deploy = length(var.enable_manifests) == 0 ? local.manifest_files_map : {
-    for file in local.manifest_files :
-    file => lookup(var.enable_manifests, file, lookup(var.enable_manifests, basename(file), true))
-  }
-}
+#   # Determine which manifests to deploy based on enable_manifests variable
+#   # If enable_manifests is empty, use all manifests from manifest_files_map
+#   # Otherwise, overlay the enable_manifests values onto manifest_files_map
+#   manifests_to_deploy = length(var.enable_manifests) == 0 ? local.manifest_files_map : {
+#     for file in local.manifest_files :
+#     file => lookup(var.enable_manifests, file, lookup(var.enable_manifests, basename(file), true))
+#   }
+# }
 
-# Read and decode each manifest file
+# # Read and decode each manifest file
+# locals {
+#   dummy_manifest = provider::kubernetes::manifest_decode_multi("")
+
+#   decoded_manifests = flatten([
+#     for file in local.manifest_files :
+#     (
+#       lookup(local.manifests_to_deploy, file, false)
+#       ? provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}"))
+#       : local.dummy_manifest
+#     )
+#   ])
+# }
+
 locals {
+  manifest_files = var.kubernetes_manifests_enabled ? fileset(var.manifest_directory, "*.yaml") : []
   decoded_manifests = flatten([
     for file in local.manifest_files :
-    lookup(local.manifests_to_deploy, file, false) == true ?
-    provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}")) : []
+    provider::kubernetes::manifest_decode_multi(file("${var.manifest_directory}/${file}"))
   ])
 }
 
@@ -35,12 +48,6 @@ resource "kubernetes_manifest" "resources" {
   manifest = each.value
 }
 
-# Output the applied resources
 output "applied_manifests" {
   value = { for k, v in kubernetes_manifest.resources : k => v.object }
-}
-
-# Output the list of manifests that were enabled
-output "enabled_manifests" {
-  value = [for file, enabled in local.manifests_to_deploy : file if enabled]
 }
