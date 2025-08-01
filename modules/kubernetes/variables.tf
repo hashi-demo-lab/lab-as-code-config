469d3000c7dbf29a67082b65a988a222bc267c2c
Aaron Evans <aarone@outlook.com.au>
2025-08-01 11:49:34 +1000

    Add variables for Next.js application and TLS certificate configuration

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -14,3 +14,33 @@ variable "vault_server_url" {
   type        = string
   default     = "https://vault.vault.svc.cluster.local:8200"
 }
+
+variable "nextjs_image" {
+  description = "Docker image for Next.js application"
+  type        = string
+  default     = "hashibank-nextjs"
+}
+
+variable "nextjs_tag" {
+  description = "Docker image tag for Next.js application"
+  type        = string
+  default     = "latest"
+}
+
+variable "app_domain" {
+  description = "Domain name for the application"
+  type        = string
+  default     = "caddy.hashibank.com"
+}
+
+variable "cert_issuer" {
+  description = "Certificate issuer for TLS certificates"
+  type        = string
+  default     = "vault-issuer"
+}
+
+variable "app_domain_secret" {
+  description = "Secret name for domain TLS certificate"
+  type        = string
+  default     = "caddy-hashibank-tls"
+}
